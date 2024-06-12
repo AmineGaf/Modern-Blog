bed27435bc25b04ffe3f2c204544f5f518470d8c
Amine Gafsi <amine.gafsi.2002@gmail.com>
2024-06-12 19:09:53 +1300

    latest version

--- src/pages/posts/js/slug.js
+++
@@ -1,7 +0,0 @@
-const readMoreBtn = document.getElementById('read-more-btn');
-const hiddenContent = document.getElementById('hidden-content');
-
-readMoreBtn.addEventListener('click', () => {
-  hiddenContent.classList.toggle('hidden');
-  readMoreBtn.style.display = 'none';
-});
